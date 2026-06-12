d5aeeb829c021a1edd12913e893ed0d838c1f6ab
Ahmad Andika <ahmdaka06@gmail.com>
2026-06-12 20:40:29 +0700

    feat: seed default owner/manager/cashier users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->owner()->create([
+            'name' => 'Owner',
+            'email' => 'owner@pos.test',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory()->manager()->create([
+            'name' => 'Manager',
+            'email' => 'manager@pos.test',
+        ]);
+
+        User::factory()->cashier()->create([
+            'name' => 'Cashier',
+            'email' => 'cashier@pos.test',
         ]);
     }
 }
