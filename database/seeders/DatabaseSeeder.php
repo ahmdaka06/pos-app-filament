0b9b5d6733cfcf54bc0905222a2294bd4f69c788
Ahmad Andika <ahmdaka06@gmail.com>
2026-06-12 20:51:35 +0700

    feat: seed default payment methods and store settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\PaymentMethod;
+use App\Models\StoreSetting;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -28,6 +30,17 @@ class DatabaseSeeder extends Seeder
         User::factory()->cashier()->create([
             'name' => 'Cashier',
             'email' => 'cashier@pos.test',
+        ]);
+
+        PaymentMethod::firstOrCreate(['code' => 'cash'], ['name' => 'Cash']);
+        PaymentMethod::firstOrCreate(['code' => 'transfer'], ['name' => 'Bank Transfer']);
+        PaymentMethod::firstOrCreate(['code' => 'qris'], ['name' => 'QRIS']);
+
+        StoreSetting::firstOrCreate([], [
+            'store_name' => 'POS Store',
+            'currency' => 'IDR',
+            'tax_percent' => 0,
+            'invoice_prefix' => 'INV',
         ]);
     }
 }
